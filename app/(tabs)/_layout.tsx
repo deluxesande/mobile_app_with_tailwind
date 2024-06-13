79dbf192220afcd005b592395478de1cd9072df0
deluxesande <deluxesande@gmail.com>
2024-06-14 00:52:19 +0300

    (Add): basic layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,79 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs, useRouter } from "expo-router";
+import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const router = useRouter();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    // TODO - make the header icons a custom component
+
+    return (
+        <>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: "#777",
+                    tabBarStyle: {
+                        height: 70,
+                        paddingBottom: 10,
+                    },
+                    headerShadowVisible: false,
+                    headerTitle: "",
+                    headerStyle: {
+                        backgroundColor: "transparent",
+                    },
+                    headerRight: () => (
+                        <Ionicons
+                            name="menu"
+                            size={28}
+                            style={{ marginHorizontal: 10 }}
+                            onPress={() => {}}
+                        />
+                    ),
+                }}
+            >
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        title: "Home",
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabBarIcon
+                                name={focused ? "home" : "home-outline"}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        title: "Profile",
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabBarIcon
+                                name={focused ? "person" : "person-outline"}
+                                color={color}
+                            />
+                        ),
+                        headerLeft: () => (
+                            <Ionicons
+                                name="chevron-back"
+                                size={28}
+                                style={{ marginHorizontal: 10 }}
+                                onPress={() => router.back()}
+                            />
+                        ),
+                        headerRight: () => (
+                            <Ionicons
+                                name="share"
+                                size={28}
+                                style={{ marginHorizontal: 10 }}
+                                onPress={() => {}}
+                            />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </>
+    );
 }
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const Profile = () => {
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "light" }}>
+            <Text>Profile</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Profile;
