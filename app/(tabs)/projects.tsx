7a7ccb8d75344f3366e346bcdae9fe92e5ffb3a1
deluxesande <deluxesande@gmail.com>
2024-06-16 00:01:48 +0300

    (Add): fonts

--- app/(tabs)/projects.tsx
+++ app/(tabs)/projects.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, ScrollView, Text } from "react-native";
 import React from "react";
 
 const Projects = () => {
     return (
-        <View>
-            <Text>Projects</Text>
-        </View>
+        <SafeAreaView className="flex-1 bg-white">
+            <ScrollView className="mx-4">
+                <Text>Projects</Text>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
