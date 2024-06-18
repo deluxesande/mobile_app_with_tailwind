536c31b25b99faed5442f76a6816b946908504c5
deluxesande <deluxesande@gmail.com>
2024-06-19 02:14:43 +0300

    (Update): notifications cards

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,12 +1,28 @@
+import CustomText from "@/components/CustomText";
 import NotificationCard from "@/components/Notifications/NotificationCard";
+import colors from "@/constants/colors";
+import fonts from "@/constants/fonts";
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
-const notifications = [1, 2, 3];
+const notifications = [1, 2, 3, 4];
 
 const Notifications = () => {
     return (
-        <ScrollView className="mx-4">
+        <ScrollView
+            className="px-4"
+            showsVerticalScrollIndicator={false}
+            style={{ backgroundColor: colors.white }}
+        >
+            <View className="flex-row items-center justify-between w-full my-4">
+                <Text
+                    className="text-3xl"
+                    style={{ fontFamily: fonts.RobotoBold }}
+                >
+                    Notifications
+                </Text>
+                <CustomText styles="text-base text-gray-500" text="Clear all" />
+            </View>
             {notifications.map((notification, index) => (
                 <NotificationCard key={index} />
             ))}
--- components/Notifications/NotificationCard.tsx
+++ components/Notifications/NotificationCard.tsx
@@ -1,10 +1,35 @@
-import { View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
+import { Image, Text, View } from "react-native";
+import CustomText from "../CustomText";
+import colors from "@/constants/colors";
 
 const NotificationCard = () => {
     return (
-        <View>
-            <Text>NotificationCard</Text>
+        <View
+            className="w-full h-20 flex-1 flex-row items-center justify-between mb-2 px-4 rounded-md shadow shadow-slate-400"
+            style={{ backgroundColor: colors.main }}
+        >
+            <View className="">
+                <Image
+                    source={require("@/assets/images/profile.png")}
+                    resizeMode="contain"
+                    className="w-10 h-16"
+                />
+            </View>
+            <View className="flex-1 flex-row ml-4 items-center justify-between">
+                <View className="items-start flex-1">
+                    <Text>Zoe sent you $20</Text>
+                </View>
+                <View className="flex-row items-center justify-end">
+                    <MaterialCommunityIcons
+                        name="timer-outline"
+                        size={15}
+                        color={"#777"}
+                    />
+                    <CustomText styles="text-sm ml-2" text="Just now" />
+                </View>
+            </View>
         </View>
     );
 };
